53c670c069ce47e7985d9fefb669a7b3e23e51f0
Nico4O4 <nicopfin@bluewin.ch>
2025-12-20 01:39:22 +0100

    i coded my first C# script without Unity :D

---
+++ first_C#_programm/first_C#_programm/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hi C#");
+Console.WriteLine(2 + 2);
+Console.WriteLine(5 - 5);
+Console.WriteLine(10 * 10);
+Console.WriteLine(100 / 100);
+
+Console.WriteLine("Das Ergebnis von 10 + 5 * 2 ist: " + " genau diese Zahl"); //zahlen in eine zeile zusammen rechnen
