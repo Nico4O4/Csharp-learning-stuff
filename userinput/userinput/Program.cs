499207e9d23e05d3fb63d19a23fafed26eb36f8a
Nico4O4 <nicopfin@bluewin.ch>
2025-12-29 16:29:01 +0100

    made a calculator

---
+++ userinput/userinput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+Console.Write("Enter first number >> ");
+
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number >> ");
+
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int final; // für speicherung gesamt ergebnis
+final = SecondNumber + FirstNumber; //nnummern übergabe an final
+
+
+Console.WriteLine("Your Nurmbers + are: " + final);
+
+//Calculator coded in C# by: Nico4O4
+/*--ideen-- 
+ * mehrere zahlen
+ * meherer operatoren
+ * vergleichs test < oder >
+ * in while true setzen damit es nicht schliesst nach ergebnis
+ * fehler abfangen
+ */
