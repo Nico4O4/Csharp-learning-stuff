99ca10e2a9dd260c0065699ae921ee187ce1358a
Nico4O4 <nicopfin@bluewin.ch>
2026-01-01 19:27:28 +0100

    just experimenting with bool stuff

---
+++ boolean_app/boolean_app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+int first_number = 600;
+int second_number = 80;
+bool final1;
+
+//first_number > second_number = final1
+
+if (first_number > second_number)
+{
+    Console.WriteLine(first_number  > second_number);//extra test?
+}
+
+else
+{
+    Console.WriteLine();
+}
+
+//still working and experimenting with booleann
